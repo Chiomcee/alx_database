e4deedc6e838b8ec0912bb48cc2735c110c1906d
Chioma Igwe <cigwe072013@gmail.com>
2023-08-13 19:53:27 -0700

    full desc of the table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
--- prints the full description of the table first_table
--- from the hbtn_0c_0 database on your MySQl server
--- The database name is passed as an argument to the MySQL command
+-- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
 
 SHOW CREATE TABLE first_table;
