b74ce0d5f33d46ec4ec8372a977708900d48ed69
Chioma Igwe <cigwe072013@gmail.com>
2023-08-13 18:54:37 -0700

    list all rows of the table first_table

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- list all rows of the table first_table from the hbtn_0c_0 database on your MySQL server
+-- All fields should be printed
+-- The database is passed as an argument to the mysql command
+
+SELECT * FROM first_table;
