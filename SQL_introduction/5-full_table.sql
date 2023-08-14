cbdca4cdfb84c4ca9464d3549481f58b4f43c3d7
Chioma Igwe <cigwe072013@gmail.com>
2023-08-13 18:45:58 -0700

    prints full description of the table first_table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- prints the full description of the table first_table
+-- from the hbtn_0c_0 database on your MySQl server
+-- The database name is passed as an argument to the MySQL command
+-- Do not use DESCRIBE or EXPLAIN statements
+
+SHOW CREATE TABLE first_table;
